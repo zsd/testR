8dcf41ad5194a3545a0c4f93cff5b3d79ce715e8
gitworkspace <zsd12379@sina.com>
2018-04-07 21:53:38 +0800

    用户模块测试用例提交.

---
+++ src/test/java/com/vansec/user/data/UserDataProvider.java
@@ -0,0 +1,36 @@
+package com.vansec.user.data;
+
+import com.vansec.comm.DataUtils;
+import com.vansec.user.domain.User;
+
+/**
+ * 用户测试数据提供类
+ */
+public class UserDataProvider {
+
+    private UserDataProvider() {}
+
+    public static User getUser1() {
+        User user = new User();
+        user.setId(DataUtils.ID_1);
+        user.setName("测试用户_001");
+        user.setLoginName("testuser");
+        user.setPassword("123");
+        user.setPhone("18681891234");
+        return user;
+    }
+
+    public static User getUser2() {
+        User user = new User();
+        user.setId("UTU_002");
+        user.setName("测试用户_002");
+        return user;
+    }
+
+    public static User getUser3() {
+        User user = new User();
+        user.setId("UTU_003");
+        user.setName("测试用户_003");
+        return user;
+    }
+}
---
+++ src/test/java/com/vansec/user/service/UserServiceTest.java
@@ -0,0 +1,75 @@
+package com.vansec.user.service;
+
+import com.vansec.AbstractTest;
+import com.vansec.comm.DataUtils;
+import com.vansec.user.data.UserDataProvider;
+import com.vansec.user.domain.User;
+import com.vansec.user.service.UserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+/**
+ * 岗位服务测试用例.
+ * @author zhousd
+ */
+public class UserServiceTest extends AbstractTest {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 保存测试.
+     */
+    @Test
+    public void saveTest(){
+        User user = UserDataProvider.getUser1();
+        userService.save(user);
+    }
+
+    /**
+     * 查询测试.
+     */
+    @Test
+    public void getByIdTest(){
+        User user = userService.getById(DataUtils.ID_1);
+        Assert.notNull(user);
+    }
+
+    /**
+     * 更新测试.
+     */
+    @Test
+    public void updateTest(){
+        User user = userService.getById(DataUtils.ID_1);
+        user.setPhone("1232132");
+        userService.update(user);
+    }
+    /**
+     * 更新测试.
+     */
+    @Test
+    public void deleteTest(){
+        userService.delete(DataUtils.ID_1);
+    }
+
+
+    /**
+     * **********************************************
+     *                业务需要
+     * **********************************************
+     */
+
+    /**
+     * 添加管理员，admin、123.
+     */
+    @Test
+    public void saveAdminTest(){
+        User user = UserDataProvider.getUser1();
+        user.setId(null);
+        user.setLoginName("admin");
+        user.setName("管理员");
+        userService.save(user);
+    }
+
+}
